a3680161e2599b47dee9fe694ac444ad42a4071c
j-hha <j-hha@bla.bla>
2017-05-18 15:57:30 -0400

    created user model and added gem bycrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users do
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
   resources :relations
   resources :occasions
   resources :excuses
