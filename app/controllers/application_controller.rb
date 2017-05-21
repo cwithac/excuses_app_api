a833be39fe9fbbf40e6c44a0e4038d534413dc65
Cathleen Wright <cathleenmwright@gmail.com>
2017-05-21 11:21:48 -0400

    BackEnd While User Logged in to Ad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,12 @@ class ApplicationController < ActionController::API
     puts "params: #{params[:id]}"
     render json: {status: 401, message: 'Unauthorized'} unless get_current_user.id == params[:id].to_i
   end
+
+  def authorize_excuse_action
+      puts "AUTHORIZE EXCUSE ACTION"
+      puts "user id: #{get_current_user.id}"
+      puts "params: #{excuse_params[:user_id]}"
+      render json: {status: 401, message: 'Unauthorized'} unless get_current_user.id == excuse_params[:user_id].to_i
+    end
+
 end
