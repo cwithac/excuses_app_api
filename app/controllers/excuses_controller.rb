13f462b9a708c310e36557af3a39eb2704ffdaa4
Cathleen Wright <cathleenmwright@gmail.com>
2017-05-21 10:50:52 -0400

    Commented Out

--- app/controllers/excuses_controller.rb
+++ app/controllers/excuses_controller.rb
@@ -1,6 +1,8 @@
 class ExcusesController < ApplicationController
   before_action :set_excuse, only: [:show, :update, :destroy]
-  # before_action :authorize_user, except: [:index, :update, :destroy]
+  # before_action :authenticate_token, except: [:index]
+  # before_action :authorize_user, except: [:index, :destroy]
+
 
   # GET /excuses
   def index
