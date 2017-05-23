673515b3759a44bdb8ad692e0af870ec1cc16a20
j-hha <j-hha@bla.bla>
2017-05-22 23:35:13 -0400

    commented out seed data - except occasions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Excuse.create([
-    { content: "The dog ate it.", count: 1 },
-    { content: "My computer has a virus and so do all my pens and pencils.", count: 1 },
-    { content: "My sister’s friend’s mother’s grandpa’s brother’s grandson’s uncle’s fish drowned.", count: 1 },
-    { content: "I forgot.", count: 1 },
-    { content: "I overslept.", count: 1 },
-    { content: "I had to organize decorations for my dog’s birthday party.", count: 1 },
-    { content: "I was there the entire time, you just didn’t notice me.", count: 1 },
-    { content: "I’ve fallen and I can’t get up.", count: 1 },
-    { content: "I don’t actually like you.", count: 1 },
-    { content: "Once I realized you weren’t going to set me up with your brother/sister, I didn’t need to talk to you anymore.", count: 1 }
-]);
+#
+# Excuse.create([
+#     { content: "The dog ate it.", count: 1 },
+#     { content: "My computer has a virus and so do all my pens and pencils.", count: 1 },
+#     { content: "My sister’s friend’s mother’s grandpa’s brother’s grandson’s uncle’s fish drowned.", count: 1 },
+#     { content: "I forgot.", count: 1 },
+#     { content: "I overslept.", count: 1 },
+#     { content: "I had to organize decorations for my dog’s birthday party.", count: 1 },
+#     { content: "I was there the entire time, you just didn’t notice me.", count: 1 },
+#     { content: "I’ve fallen and I can’t get up.", count: 1 },
+#     { content: "I don’t actually like you.", count: 1 },
+#     { content: "Once I realized you weren’t going to set me up with your brother/sister, I didn’t need to talk to you anymore.", count: 1 }
+# ]);
 
 Occasion.create([
   { title: "Missed deadline" },
@@ -25,18 +25,18 @@ Occasion.create([
   { title: "Wrongdoing to another" }
 ]);
 
-Relation.create([
-  { occasion_id: 1, excuse_id: 1 },
-  { occasion_id: 1, excuse_id: 2 },
-  { occasion_id: 1, excuse_id: 3 },
-  { occasion_id: 2, excuse_id: 3 },
-  { occasion_id: 1, excuse_id: 4 },
-  { occasion_id: 2, excuse_id: 4 },
-  { occasion_id: 1, excuse_id: 5 },
-  { occasion_id: 2, excuse_id: 5 },
-  { occasion_id: 2, excuse_id: 6 },
-  { occasion_id: 2, excuse_id: 7 },
-  { occasion_id: 2, excuse_id: 8 },
-  { occasion_id: 3, excuse_id: 9 },
-  { occasion_id: 3, excuse_id: 10 }
-]);
+# Relation.create([
+#   { occasion_id: 1, excuse_id: 1 },
+#   { occasion_id: 1, excuse_id: 2 },
+#   { occasion_id: 1, excuse_id: 3 },
+#   { occasion_id: 2, excuse_id: 3 },
+#   { occasion_id: 1, excuse_id: 4 },
+#   { occasion_id: 2, excuse_id: 4 },
+#   { occasion_id: 1, excuse_id: 5 },
+#   { occasion_id: 2, excuse_id: 5 },
+#   { occasion_id: 2, excuse_id: 6 },
+#   { occasion_id: 2, excuse_id: 7 },
+#   { occasion_id: 2, excuse_id: 8 },
+#   { occasion_id: 3, excuse_id: 9 },
+#   { occasion_id: 3, excuse_id: 10 }
+# ]);
